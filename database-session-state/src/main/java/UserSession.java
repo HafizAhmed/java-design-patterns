8de0f1d942e649a0ca6499fb6ee782c633cd2787
Hafiz Ahmed <hafiz.ahmed@oracle.com>
2021-11-18 23:20:42 -0800

    add database session state pattern

---
+++ database-session-state/src/main/java/UserSession.java
@@ -0,0 +1,51 @@
+import Database.DatabaseConnection;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+
+public class UserSession implements DatabaseSession {
+
+    private String sessionID;
+    private Timestamp creationTime;
+    private Timestamp lastAccessTime;
+    private Preference preference;
+
+    public String getSessionID() {
+        return sessionID;
+    }
+
+    public Timestamp getCreationTime() {
+        return creationTime;
+    }
+
+    public Timestamp getLastAccessTime() {
+        return lastAccessTime;
+    }
+
+    public void setSessionID(String sessionID) {
+        this.sessionID = sessionID;
+    }
+
+    public void setCreationTime(Timestamp creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public void setLastAccessTime(Timestamp lastAccessTime) {
+        this.lastAccessTime = lastAccessTime;
+    }
+
+
+    @Override
+    public void setAttribute(Preference value) throws SQLException {
+        this.preference = value;
+
+    }
+
+    @Override
+    public Preference getAttribute() {
+        return this.preference;
+    }
+
+
+
+}
